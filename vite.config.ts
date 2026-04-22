1b842ae6a381a746998a334e32910b32ea4d53e5
Bailey Ryan <bailey@progyny.com>
2026-04-22 16:53:03 -0400

    Add base path for GitHub Pages deployment to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TOA_Request_Formspree/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
